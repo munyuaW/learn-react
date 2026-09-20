ddad8f56c05c9eb9109070424856055ff0376edc
munyua <mrmunyuajm@gmail.com>
2026-09-20 11:14:02 +0300

    create and render a simple button component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ export default function App() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold">Welcome to Learnig React</h1>
+      <MyButton />
     </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button className="px-2 py-1.5 bg-cyan-500 rounded-lg text-white mt-2">
+      I am a button
+    </button>
   );
 }
