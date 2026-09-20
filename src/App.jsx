ffc8c59f2dcd5cc7e3f5e657cd6cb02f49689d89
munyua <mrmunyuajm@gmail.com>
2026-09-20 11:37:04 +0300

    displaying data: using curly braces to escape to js in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
 export default function App() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center space-y-2">
       <h1 className="text-3xl font-bold">Welcome to Learnig React</h1>
       <MyButton />
+      <About />
+      <Profile />
     </div>
   );
 }
 
+// Simple component
 function MyButton() {
   return (
-    <button className="px-2 py-1.5 bg-cyan-500 rounded-lg text-white mt-2">
+    <button className="px-2 py-1.5 bg-cyan-500 rounded-lg text-white">
       I am a button
     </button>
+  );
+}
+
+// Displaying data
+// Using curly braces to escape back into js to embed variables from code
+const myName = "Munyua";
+
+function About() {
+  return <h3 className="text-cyan-950 font-bold">My name is {myName}</h3>;
+}
+
+// profile: complex expressions inside jsx
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://react.dev/images/docs/scientists/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <div className="p-2 border border-gray-300 rounded-sm">
+      <h1>{user.name}</h1>
+      <img
+        className={`size=[${user.imageSize}] rounded-full`}
+        src={user.imageUrl}
+        alt={`Photo of ${user.name}`}
+      />
+    </div>
   );
 }
