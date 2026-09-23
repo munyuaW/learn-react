6448be4b9e68ff0c9cb0a326c02ccf714436fbcc
munyua <mrmunyuajm@gmail.com>
2026-09-23 20:18:56 +0300

    build game board layout

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,5 +1,41 @@
-function App() {
-  return <h1>Tic Tac Toe</h1>;
+import { useState } from "react";
+
+export default function App() {
+  return <Board />;
 }
 
-export default App;
+function Board() {
+  return (
+    <div className="mt-5 ml-10">
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+}
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button
+      className="text-2xl font-bold border border-gray-400 flex justify-center items-center w-8 h-8"
+      onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
