7d2d8ce208be8bf8ff238c997905c0255d369f8e
munyua <mrmunyuajm@gmail.com>
2026-09-23 20:38:25 +0300

    lifting state Square child component to the parent Board

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -5,36 +5,31 @@ export default function App() {
 }
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <div className="mt-5 ml-10">
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </div>
   );
 }
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("X");
-  }
+function Square({ value }) {
   return (
-    <button
-      className="text-2xl font-bold border border-gray-400 flex justify-center items-center w-8 h-8"
-      onClick={handleClick}>
+    <button className="text-2xl font-bold border border-gray-400 flex justify-center items-center w-8 h-8">
       {value}
     </button>
   );
