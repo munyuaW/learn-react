7153cb7336d02eecd04c2d669366b9bf2cdb5fdc
munyua <mrmunyuajm@gmail.com>
2026-09-23 22:04:37 +0300

    add click handler

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -6,30 +6,33 @@ export default function App() {
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = () => {};
   return (
     <div className="mt-5 ml-10">
       <div className="flex">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="flex">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="flex">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </div>
   );
 }
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
-    <button className="text-2xl font-bold border border-gray-400 flex justify-center items-center w-8 h-8">
+    <button
+      className="text-2xl font-bold border border-gray-400 flex justify-center items-center w-8 h-8"
+      onClick={onSquareClick}>
       {value}
     </button>
   );
